f86d71c5c75d6ac7e1350aeeae03900c9cca03b9
Tor Kingdon <tor@hearkitty.com>
2021-04-20 00:16:07 -0600

    very close at bedtime

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 # app.py written entirely by Tor Kingdon
 
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session, make_response
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import surveys
 from helpers import survey_size
-import globals
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "big-secret"
@@ -12,97 +11,125 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-@app.route('/')
-def landing_page():
-	"""reset global variables
-	if there are not availble surveys left for this user, send a message to that effect
-	display either message or avaiables surveys for user to choose from"""
-	globals.responses = []
-	globals.types = []
 
-	if len(surveys) == len(globals.completed_surveys):
+@app.route('/')
+def set_up_session():
+	"""set up Flask session to store user's progress separately from anyone else's"""
+
+	# if user has saved info in a session, keep it. if not, start with default values to be replaced later
+
+	if not session.get('active_survey'):
+		session['active_survey_key'] = 'dummy'
+	if not session.get('completed_surveys'):
+		session['completed_surveys'] = ['dummy']
+	if not session.get('responses'):
+		session['responses'] = []
+	return redirect('/home')
+	# return render_template('home.html')
+
+@app.route('/home')
+def build_home_html():
+	if len(surveys) == len(session['completed_surveys']):
 		flash("THANK YOU. You have completed all of our surveys.", "info")
 		flash("Please check back later to see if there are any new surveys", "info")
 	
-	return render_template('home.html', surveys = surveys, completed_surveys = globals.completed_surveys)
+	return render_template('home.html', surveys = surveys)
 
-# if we're GETting a request, it's not from our forms
-@app.route('/question/<url_pt2>', methods=['GET'])
-def this_is_not_my_beautiful_page(url_pt2):
-	"""if we are receiving a GET request on this page, it did not come from one of our forms and needs to be dealt with accordingly"""
-	if url_pt2.isnumeric():
-		where_are_we = len(globals.responses)
-		question = int(url_pt2)
-		if question == where_are_we:
-			try:
-				flash(globals.responses_flash)
-				return render_template('question.html', question_id = question, question_num = question +1, survey = globals.active_survey, key = globals.survey_key, columns = globals.columns)
-			except: 
-				flash("insuffienct data to continue", "error")
-				flash("please choose a survey", "info")
-				return redirect('/')
 
-	flash("bad url entered", "error")
-	flash("please choose a survey", "info")
-	return redirect('/')
 	
-@app.route('/question/<int:question>', methods=['POST'])
-def display_next_question(question):
+@app.route('/question/<url_pt2>', methods=['POST', 'GET'])
+def display_next_question(url_pt2):
 	"""process previous page and move on to next question"""
-	where_are_we = len(globals.responses)
+		
+	# if the URL is non-numeric, it was manually entered and must go back to the start
+	if not url_pt2.isnumeric():
+		flash("bad url", "error")
+		flash("insuffienct data to continue", "error")
+		flash("please choose a survey", "info")
+		return redirect('/')
 
-	globals.survey_key = request.form['key']
-	# determine the number of the page we should be on
-
-	# if this is the first question of the selected survey
+	# convert the URL to an integer
+	question = int(url_pt2)
+	# determine the question we need next
+	where_are_we = len(session.get('responses'))
+	# is this the first question after choosing the survey?
 	if where_are_we == 0 and question == 0:
-		globals.survey_key = request.form['key']
-		globals.active_survey = surveys[globals.survey_key]
-		(globals.num_questions, globals.columns) = survey_size(globals.active_survey)
-		return render_template('question.html', question_id = question, question_num = question + 1, survey = globals.active_survey, key = globals.survey_key, columns = globals.columns)
+		chosen_key = request.form['key']
+		session['active_survey_key'] = chosen_key
+		active_survey = surveys[chosen_key]
+		(num_questions, columns) = survey_size(active_survey)
+		try:
+			# if surveys.values().get(active_survey):
+			return render_template('question.html', question_id = question, question_num = question + 1, survey = active_survey, key = session['active_survey_key'], columns = columns)
+		except:
+			flash ("we can't find a chosen survey", "error")
+			flash ("please try again", "info")
+			return redirect('/')
+	
+	# are we on a valid question URL (i.e. the # matches the next one we need)
+	if where_are_we == question:
+		try:
+			return render_template('question.html', question_id = question, question_num = question + 1, survey = active_survey, key = session['active_survey_key'], columns = columns)
+		except:
+			flash("bad url", "error")
+			flash("insuffienct data to continue", "error")
+			flash("please choose a survey", "info")
+			return redirect('/')
+
+
+	# get the size of the chosen survey:
+	active_survey = surveys[session['active_survey_key']]
+	(num_questions, columns) = survey_size(active_survey)
 
 	# redirect to home page if a user manually navigates to a question page wihtout selecting a survey:
-	if globals.survey_key == "dummy":
+	if session.get('survey_key') == "dummy":
+		flash('is this where it failed?', 'troubleshooting')
+		# YES, but when did the key get turned back into dummy?
 		flash("a survey was never chosen", "error")
 		flash("please choose a survey", "info")
 		return redirect('/')
 
 	# we're only at this point if a question has just been answered
-	if not request.form['choice']:
+	if not request.form.get('choice'):
 		flash("no choice was made", "error")
 		flash("please select an answer", "info")
-		return render_template('question.html', question_id = question - 1, question_num = question, survey = globals.active_survey, key = globals.survey_key, columns = globals.columns)
+		return render_template('question.html', question_id = question, question_num = question + 1, survey = active_survey, key = session['active_survey_key'], columns = columns)
 
-	# was there a text field in the question?
-	if not request.form.get('elaboration'):
-		globals.responses.append(request.form['choice'])
-	elif request.form.get('elaboration') and request.form.get('choice'):
-		globals.responses.append([request.form['choice'], request.form['elaboration']])
-		globals.allowing_text.append(question - 1)
+	# enter answer (and text) into session['responses']
+	if request.form.get('elaboration'):
+		session['responses'].append([request.form['choice'], request.form['elaboration']])
+		if session.get('allowing_text'):
+			session['allowing_text'].append(question - 1)
+		else:
+			session['allowing_text'] = [question - 1]
 	else:
-		flash("bad data on form", "error")
-		flash("please try this question again", "info")
-		return redirect(f'/question/{where_are_we}')
+		try:
+			session['responses'].append(request.form['choice'])
+		except:
+			flash("bad data on form", "error")
+			flash("please try this question again", "info")
+			return redirect(f'/question/{where_are_we}')
 
 	# if user just answered the last question of the survey:
-	if len(globals.responses) == globals.num_questions:
-		# globals.survey_done = True
-		globals.completed_surveys.append(globals.survey_key)
-		# for i in range(globals.num_questions):
-		# 	globals.types.append(type(globals.responses[i]))
+	if question == len(active_survey.questions):
+		session['completed_surveys'].append(session['active_survey_key'])
 		return redirect('/response')
 
 	# user has at least one more question to answer
-	return render_template('question.html', question_id = question, question_num = question +1, survey = globals.active_survey, key = globals.survey_key, columns = globals.columns)
+	return render_template('question.html', question_id = question, question_num = question +1, survey = active_survey, key = session['active_survey_key'], columns = columns)
 
 @app.route('/response')
 def survey_done():
-	# try:
-	return render_template('response.html', survey = globals.active_survey, responses = globals.responses, num_questions = globals.num_questions, columns = globals.columns, allowing_text = globals.allowing_text)
-	# except:
-	# 	flash("something went wonky in this survey", "error")
-	# 	flash("please try again or pick another one", "info")
-	# 	return redirect('/')
+	active_survey = surveys[session['active_survey_key']]
+	(num_questions, columns) = survey_size(active_survey)
+	try:
+		return render_template('response.html', survey = active_survey, num_questions = num_questions, columns = columns)
+	except:
+		flash("something went wonky in this survey", "error")
+		flash("it's probably my bad", "info")
+		flash("please try again or pick another one", "info")
+		flash("just don't hate me", "info")
+		return redirect('/')
 
 @app.route('/reset')
 def reset_and_restart():
