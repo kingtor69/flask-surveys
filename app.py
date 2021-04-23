a0ec2b3348f3289802c66b096633f5554f2f6d29
Tor Kingdon <tor@hearkitty.com>
2021-04-23 15:55:55 -0600

    done?

--- app.py
+++ app.py
@@ -2,9 +2,7 @@
 
 from flask import Flask, request, render_template, redirect, flash, session, make_response
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import surveys
-from helpers import survey_size
-# import globals
+from surveys import surveys, survey_size
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "big-secret"
@@ -21,36 +19,59 @@ def phone_home():
 @app.route('/home')
 def build_home_html():
 	completed_surveys = session.get('completed_surveys', [])
+	session.clear()
 	session['completed_surveys'] = completed_surveys
-	
 	return render_template('home.html', surveys = surveys)
 
-@app.route('/question/0', methods = ['POST', 'GET'])
-def question_zero:
-	return render_template('question.html')
 
-# @app.route('/question/<int:question>', methods=['POST', 'GET'])
-# def display_next_question(question):
-# 	if not session.get('survey_key'):
-# 		try:
-# 			session['survey_key'] = request.form['survey'])
-# 		except:
-# 			flash('please choose a survey', 'info')
-# 			return redirect('/home')
+@app.route('/question/<int:question>', methods=['POST', 'GET'])
+def display_next_question(question):
+	"""if there is no form data, redirect to previous page
+	otherwise, process form data and ask next question"""
+	if not session.get('survey_key'):
+		try:
+			session['survey_key'] = request.form['key']
+		except:
+			flash('no survey choice registered', 'error')
+			flash('please choose a survey', 'info')
+			return redirect('/home')
+
+	if not request.form.get('choice'):
+		flash('no response was registered for this question', 'error')
+		flash('please try again', 'info')
+		return redirect(f'/question/{question}')
+
+	survey_key = session['survey_key']
+	survey = surveys[survey_key]
+	# we're ready to proceed if this was question 0
+	(num_questions, columns) = survey_size(survey)
+	has_text = (session.get('has_text', []))
+	has_text.append(False)
+	responses = session.get('responses', [])
+
+	if question > 0:
+		# does question have a text box?
+		if survey.questions[question].allow_text:
+			has_text[question] = True
+			elaboration = request.form.get('elaboration', '<left blank>')
+			response = [request.form['choice'], elaboration]
+		else:
+			response = request.form['choice']
+
+		responses.append(response)
+
+	# are we done?
+	if question == num_questions:
+		completed_surveys = session.get('completed_surveys', [])
+		completed_surveys.append(survey_key)
+		session['completed_surveys'] = completed_surveys
+		return redirect('/response')
 	
-# 	survey_key = ('survey_key')
-
-# 	responses = session.get('responses', [])
-# 	responses.append(request.form['choice'])
-# 	session['responses'] = responses
-
-# 	if question == len(surveys[survey_key])
-# 		completed_surveys = session.get('completed_surveys', [])
-# 		completed_surveys.append(survey_key)
-# 		session['completed_surveys'] = completed_surveys
-# 		return redirect('/response')
+	session['has_text'] = has_text
+	session['responses'] = reseponses
+	session['survey_key'] = survey_key
 	
-# 	return render_template('question.html, (num_questions, columns) = helpers.survey_size(survey_key)')
+	return render_template('question.html', question_id = question, question_num = question + 1, survey = survey, columns = columns)
 
 	# -- question/0
 	# 1. software saves chosen survey to session
@@ -68,17 +89,20 @@ def question_zero:
 
 	# question = len(survey.questions)
 	# 7. on last question, redirect to a finished page
-	return render_template('question.html', survey_key = survey_key, question_id = question, question_num = question + 1)
+
 
 
 @app.route('/response')
 def survey_done():
 	""" survey is complete, load thank you page with summary of user's answers"""
-
-	# 8. mark the survey as complete in session data
-	# 9. thank the user and display their answers
-	# 10. redirect them back to home page
-	return render_template('response.html', survey = active_survey, num_questions = num_questions, columns = columns)
+	key = session['survey_key']
+	completed_surveys = session.get('completed_surveys', [])
+	completed_surveys.append(key)
+	survey = surveys[key]
+	(num_questions, columns) = survey_size(survey)
+	session['completed_surveys'] = completed_surveys
+	
+	return render_template('response.html', survey = survey, num_questions = num_questions, columns = columns)
 
 @app.route('/reset')
 def reset_and_restart():
